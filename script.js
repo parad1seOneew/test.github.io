980d58852b6efcfa6af3d532cb38e2e4c43240e5
parad1seOneew <svyatpri@gmail.com>
2024-07-24 18:39:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,36 @@ var levelNum = 0;
 levels = [0,100,1000,10000,100000,1000000,10000000];
 plus = 1;
 
+if (localStorage.length!=0){
+    score = Number(localStorage.getItem('score'));
+    levelNum = Number(localStorage.getItem('level'));
+}
+else{
+    localStorage.setItem('score', '0');
+    localStorage.setItem('level', '0');
+};  
 
-localStorage.setItem('score', '0');
-localStorage.setItem('level', '0');
- 
 scoreText = document.querySelector(".now-score");
 scoreText.textContent = String(score);
+
 levelNum = Number(localStorage.getItem('level'));
+
 lvltxt = document.querySelector(".lvl-txt");
+lvltxt.textContent = String(levelNum+1)+ "/" +String(levels.length-1);
 
 
 level = document.querySelector(".progress-bar-point");
-lvltxt.textContent = String(levelNum+1)+ "/" +String(levels.length-1);
+level.style.width = String(((score/levels[levelNum+1])*100))+"%";  
 
+reset = document.querySelector('.reset-but');
+reset.onclick = resetProgress;
+
+
+function resetProgress(){
+    localStorage.setItem('score', '0');
+    localStorage.setItem('level', '0');
+    location.reload();
+};
 function inc(){
     score+=plus;
     level.style.width = String(((score/levels[levelNum+1])*100))+"%";  
